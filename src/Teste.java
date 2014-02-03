6456228f12eb227e84a0ce7b3e29da1e9c31d9b0
Henrot <boris.henrot@iut-valence.fr>
2014-02-03 17:35:14 +0100

    Initial commit

---
+++ src/Teste.java
@@ -0,0 +1,15 @@
+
+public class Teste {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println(args[0]);
+	System.err.println("Goodbye !");
+	if(args.length>0)
+	    System.out.println(args[0]);
+	else
+	    System.out.println("rien");
+
+    }
+
+}
